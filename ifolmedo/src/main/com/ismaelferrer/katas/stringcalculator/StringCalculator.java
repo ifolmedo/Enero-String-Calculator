926ec7ccab27b2fc0b94528a132dbaf125511862
Ismael Ferrer <ifolmedo@gmail.com>
2011-06-28 00:59:32 +0200

    Primera versión de la kata. Implementado hasta el punto 5.

---
+++ ifolmedo/src/main/com/ismaelferrer/katas/stringcalculator/StringCalculator.java
@@ -0,0 +1,57 @@
+package com.ismaelferrer.katas.stringcalculator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+	private static final char CHANGE_DELIMITER_SUFFIX = '\n';
+	private static final String DEFAULT_DELIMITER_REGEXP = "[,\n]";
+	private static final String CHANGE_DELIMITER_PREFIX = "//";
+
+	public static int add(final String string) {
+		if (string.isEmpty()) return 0;
+		return addNonEmptyString(string);
+	}
+
+	private static int addNonEmptyString(final String string) {
+		String delimiterRegExp = DEFAULT_DELIMITER_REGEXP;
+		String processedString = string;
+		if (string.startsWith(CHANGE_DELIMITER_PREFIX)) {
+			String nonEscapedDelimiterRegExp = string.substring(
+					CHANGE_DELIMITER_PREFIX.length(), 
+					string.indexOf(CHANGE_DELIMITER_SUFFIX));
+			delimiterRegExp = Pattern.quote(nonEscapedDelimiterRegExp);
+			processedString = string.substring(
+					string.indexOf(CHANGE_DELIMITER_SUFFIX)+1);
+		}
+		return addStringArray(processedString.split(delimiterRegExp));
+
+	}
+
+	private static int addStringArray(final String[] addendsArray) {
+		int sum = 0;
+		List<Integer> illegalArguments = new ArrayList<Integer>();
+		for (String addend : addendsArray) {
+			int intAddend = Integer.parseInt(addend);
+			if (intAddend < 0) illegalArguments.add(new Integer(addend));
+			sum += intAddend;
+		}
+		if (illegalArguments.isEmpty()) return sum;
+		throw new IllegalArgumentException("negatives not allowed: " +
+				join(illegalArguments, ","));
+	}
+	
+	private static String join(final Collection<Integer> integers, 
+			final String delimiter) {
+	    if (integers.isEmpty()) return "";
+	    Iterator<Integer> iter = integers.iterator();
+	    StringBuffer buffer = new StringBuffer(iter.next().toString());
+	    while (iter.hasNext()) 
+	    	buffer.append(delimiter).append(iter.next());
+	    return buffer.toString();
+	}
+}
---
+++ ifolmedo/src/test/com/ismaelferrer/katas/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,71 @@
+package com.ismaelferrer.katas.stringcalculator;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import org.junit.Test;
+
+import com.ismaelferrer.katas.stringcalculator.StringCalculator;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void whenAddingEmptyStringThenShouldReturnZero() {
+		assertThat(StringCalculator.add(""), is(0));
+	}
+	
+	@Test
+	public void whenAddingOneNumberThenShouldReturnSameNumber() {
+		String [] sums={"1","2","10","100"};
+		int [] expectedResults ={1,2,10,100};
+		assertThamSumsResultsAreTheExpected(sums, expectedResults);
+	}
+
+
+	
+	@Test
+	public void whenAddingTwoNumbersThenShouldReturnTheSum() {
+		String [] sums={"1,2","2,10","3,70","15,100"};
+		int [] expectedResults ={3,12,73,115};
+		assertThamSumsResultsAreTheExpected(sums, expectedResults);
+	}
+	
+	@Test
+	public void whenAddingSeveralNumbersThenShouldReturnTheSum() {
+		String [] sums={"1,2,7","2,9,33,45","3,70,21,43,560"};
+		int [] expectedResults ={10,89,697};
+		assertThamSumsResultsAreTheExpected(sums, expectedResults);
+	}	
+	
+	@Test
+	public void whenUsingNewLineDelimeterThenShouldReturnTheSum() {
+		String [] sums={"1\n2,3","6\n54\n66"};
+		int [] expectedResults ={6,126};
+		assertThamSumsResultsAreTheExpected(sums, expectedResults);
+	}
+	
+	@Test
+	public void whenSelectingNewDelimeterThenShludReturnTheSum() {
+		String [] sums={"//;\n1;2;7","//+\n19+10+221"};
+		int [] expectedResults ={10,250};
+		assertThamSumsResultsAreTheExpected(sums, expectedResults);
+	}
+	
+	@Test
+	public void testAddNegativeNumbers() {
+		try {
+			StringCalculator.add("-1,-2");
+			fail("An Illegal Argument Exception should be thrown");
+		} catch (IllegalArgumentException iae){
+			assertThat(iae.getMessage(), is("negatives not allowed: -1,-2"));
+		}
+	}
+
+	private void assertThamSumsResultsAreTheExpected(String[] sums, int[] expectedResults) {
+		for(int i=0; i<sums.length; i++){		
+			int result = StringCalculator.add(sums[i]);
+			assertThat(result, is(expectedResults[i]));
+		}
+	}		
+	
+}
